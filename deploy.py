d7226b65bb9f4e2ac5de89ffe6bd795542532d7d
Dean Harding <dean@codeka.com.au>
2022-03-06 23:04:18 -0800

    Added the beginnings of a deploy script that builds everything and assembles it ready to deploy.

---
+++ deploy.py
@@ -0,0 +1,144 @@
+
+import argparse
+import os
+import platform
+import shutil
+import subprocess
+
+parser = argparse.ArgumentParser(description='Run the podcreep server locally.')
+parser.add_argument('--web_path', type=str, default='../web', help='Path where the web app is checked out to.')
+parser.add_argument('--server_path', type=str, default='.', help='Path where the server code is checked out to.')
+parser.add_argument('--android_path', type=str, default='../android', help='Path where the Android app\'s code is checked out to.')
+parser.add_argument('--deploy_path', type=str, default='../dist', help='Path where we build and deploy the server to, temporarily.')
+args = parser.parse_args()
+
+web_path = os.path.abspath(args.web_path)
+server_path = os.path.abspath(args.server_path)
+android_path = os.path.abspath(args.android_path)
+deploy_path = os.path.abspath(args.deploy_path)
+
+# In addition to .go and .py source files, which we ignore when creating the server dist folder,
+# we'll also ignore this list of files.
+SERVER_IGNORE_FILES = [
+  ".gitignore",
+  "go.mod",
+  "go.sum",
+  "LICENSE",
+  "README.md",
+]
+
+ANDROID_AAB_PATH = os.path.join(android_path, "mobile/build/outputs/bundle/release/mobile-release.aab")
+
+def build_web():
+  print(" - building web...")
+
+  # First, clear the old dist/ directory
+  dist_dir = os.path.join(web_path, 'dist')
+  for f in os.listdir(dist_dir):
+    os.remove(os.path.join(dist_dir, f))
+
+  # Now run 'ng build' to actually build the stuff.
+  subprocess.run("ng build --configuration production", cwd=web_path, check=True, shell=True)
+
+
+def copy_web():
+  print(" - copying web...")
+
+  # First, clear the old dist/ directory
+  dest_dir = os.path.join(server_path, 'dist')
+  for f in os.listdir(dest_dir):
+    os.remove(os.path.join(dest_dir, f))
+
+  # Now copy the files.
+  src_dir = os.path.join(web_path, 'dist')
+  for f in os.listdir(src_dir):
+    shutil.copy(os.path.join(src_dir, f), dest_dir)
+
+
+def build_server():
+  print(" - building server")
+
+  subprocess.run("go build", cwd=server_path, check=True, shell=True)
+
+
+def copy_server():
+  print(" - copying server")
+
+  exe_name = "server"
+  if platform.system() == "Windows":
+    exe_name += ".exe"
+
+  server_deploy_path = os.path.join(deploy_path, "server")
+  if os.path.isdir(server_deploy_path):
+    # If it already exists, delete it first.
+    shutil.rmtree(server_deploy_path)
+
+  os.makedirs(server_deploy_path)
+  shutil.move(os.path.join(server_path, exe_name), server_deploy_path)
+  for root, _, files in os.walk(server_path):
+    for f in files:
+      rel_dir = os.path.relpath(root, server_path)
+      if rel_dir.startswith(".git"):
+        # Ignore files in the .git directory
+        continue
+      _, ext = os.path.splitext(f)
+      if ext == ".go" or ext == ".py":
+        # Ignore go and python source files.
+        continue
+      src_file = f
+      if rel_dir != ".":
+        src_file = os.path.join(rel_dir, f)
+      if src_file in SERVER_IGNORE_FILES:
+        # Ignore other files in SERVER_IGNORE_FILES.
+        continue
+      dest_dir = os.path.join(server_deploy_path, rel_dir)
+      if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+      shutil.copy(os.path.join(root, f), dest_dir)
+
+
+def build_android():
+  print(" - building android")
+  print("   - clean")
+  subprocess.run([os.path.join(".", "gradlew"), "clean"], cwd=android_path, check=True, shell=True)
+  print("   - bundle")
+  subprocess.run([os.path.join(".", "gradlew"), "bundle"], cwd=android_path, check=True, shell=True)
+
+
+def get_app_version():
+  bundletool_path = os.path.join(android_path, "bundletool-all-1.8.2.jar")
+  proc = subprocess.run([
+      "java", "-jar", bundletool_path, "dump", "manifest", "--bundle", ANDROID_AAB_PATH,
+      "--xpath", "/manifest/@android:versionName"
+    ], capture_output=True, check=True)
+  version = proc.stdout.decode("utf-8")
+  return version.strip()
+
+
+def copy_android():
+  version = get_app_version()
+  dest_file = f"podcreep-{version}.aab"
+  print(" - copying to", dest_file)
+  dest_dir = os.path.join(deploy_path, "android")
+  if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+  shutil.copy(ANDROID_AAB_PATH, os.path.join(dest_dir, dest_file))
+
+
+def main():
+  print("Web code:", web_path)
+  print("Android code:", android_path)
+  print("Server code:", server_path)
+  print("Deploy path:", deploy_path)
+  input("Press Enter to begin.")
+
+  build_web()
+  copy_web()
+  build_server()
+  copy_server()
+  build_android()
+  copy_android()
+
+
+if __name__ == "__main__":
+  main()
